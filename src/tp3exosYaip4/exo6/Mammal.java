d497631378a0a1ef4b5c72e3b954b41d7bdbb7a7
Tommy Li <tommy.li@etu.univ-amu.fr>
2026-02-12 12:08:56 +0100

    Exo 4.2 et Exo 4.6

---
+++ src/tp3exosYaip4/exo6/Mammal.java
@@ -0,0 +1,12 @@
+package tp3exosYaip4.exo6;
+
+public class Mammal extends Animal {
+	public Mammal(String name) {
+		super(name);
+	}
+	
+	@Override
+	public String toString() {
+		return "Mammal[" + super.toString() + "]";
+	}
+}
